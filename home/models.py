bf1ad5382ad22eca2a6df729a27e016fc13bd976
Abhay Gupta <abhaygupta@Abhays-Mac.local>
2024-07-06 17:34:24 +0530

    Create Item Page Design Ready

--- home/models.py
+++ home/models.py
@@ -5,10 +5,10 @@ from django import forms
 class Item(models.Model):
     prof = models.ForeignKey(User, on_delete = models.CASCADE, default=None)
     item_name = models.CharField(max_length=100)
-    item_loc = models.CharField(max_length=100, blank = True)
+    item_location = models.CharField(max_length=100, blank = True)
     precise_loc = models.TextField(blank= True)
     item_desc = models.TextField(blank=True)
-    img = models.ImageField(upload_to='photos/', blank=True)
+    item_image = models.ImageField(upload_to='photos/', blank=True)
     is_published = models.BooleanField(default=True)
     def __str__(self):
         return self.item_name
